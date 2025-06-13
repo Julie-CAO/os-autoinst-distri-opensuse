7e49244300fbeadc80dd65064159bbd7d30a0398
Julie-CAO <julie_caohe@163.com>
2025-06-13 20:20:22 +0800

    Try with 'super' ahead of each send key

--- tests/virtualization/virt_install.pm
+++ tests/virtualization/virt_install.pm
@@ -27,14 +27,18 @@ sub run {
     record_info("Typed 3 times 'super'", "");
     sleep 1;
     save_screenshot;
-    send_key "alt-f2";
-    record_info("Typed 'alt-f2'", "");
+    send_key "super-alt-f2";
+    record_info("Typed 'super-alt-f2'", "");
     sleep 1;
     save_screenshot;
-    send_key "super-f2";
+    send_key "alt-f4";
+    record_info("Typed 'alt-f4'", "");
     sleep 1;
-    record_info("Typed 'super-f2'", "");
     save_screenshot;
+    send_key "super-alt-f4";
+    record_info("Typed 'super-alt-f4'", "");
+    save_screenshot;
+
 
     #send_key "shift-f12";
     x11_start_program('vncviewer :0', target_match => 'virtman-gnome_virt-install', match_timeout => 100);
