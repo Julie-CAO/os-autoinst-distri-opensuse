9b6a4d2f691549230fe9029ddc86e73a33e56217
Julie-CAO <julie_caohe@163.com>
2025-06-13 20:54:28 +0800

    Try with --noautoconsole in virt-install

--- tests/virtualization/virt_install.pm
+++ tests/virtualization/virt_install.pm
@@ -29,8 +29,7 @@ sub run {
     wait_still_screen;
     x11_start_program('vncviewer :0', target_match => 'virtman-gnome_virt-install', match_timeout => 100);
     # closing all windows
-    # julie debug
-    #send_key 'alt-f4' for (0 .. 2);
+    send_key 'alt-f4' for (0 .. 2);
     sleep 3600;
 }
 
