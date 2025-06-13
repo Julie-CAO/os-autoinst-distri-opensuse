af4703b8d7481c9e074a119689fc5c6f34219fda
Julie-CAO <julie_caohe@163.com>
2025-06-13 20:54:28 +0800

    Try with --noautoconsole in virt-install

--- tests/virtualization/virt_install.pm
+++ tests/virtualization/virt_install.pm
@@ -16,35 +16,35 @@ sub run {
     ensure_installed('virt-install');
     x11_start_program('xterm');
     become_root;
-    script_run('virt-install --name TESTING --osinfo detect=on,require=off --memory 512 --disk none --boot cdrom --graphics vnc &', 0);
+    #    script_run('virt-install --name TESTING --osinfo detect=on,require=off --memory 512 --disk none --boot cdrom --graphics vnc &', 0);
+    script_run('virt-install --name TESTING --osinfo detect=on,require=off --memory 512 --disk none --boot cdrom --graphics vnc --noautoconsole &', 0);
     wait_still_screen(15);
     if (check_screen('allow-inhibiting-shortcuts', 10)) {
 	    #        send_key('left');
         send_key('ret');
         save_screenshot;
     }
-    send_key "super" for (0 .. 2);
-    record_info("Typed 3 times 'super'", "");
-    sleep 1;
-    save_screenshot;
-    send_key "super-alt-f2";
-    record_info("Typed 'super-alt-f2'", "");
-    sleep 1;
-    save_screenshot;
-    send_key "alt-f4";
-    record_info("Typed 'alt-f4'", "");
-    sleep 1;
-    save_screenshot;
-    send_key "super-alt-f4";
-    record_info("Typed 'super-alt-f4'", "");
-    save_screenshot;
+    #    send_key "super" for (0 .. 2);
+    #    record_info("Typed 3 times 'super'", "");
+    #    sleep 1;
+    #    save_screenshot;
+    #    send_key "super-alt-f2";
+    #    record_info("Typed 'super-alt-f2'", "");
+    #    sleep 1;
+    #    save_screenshot;
+    #    send_key "alt-f4";
+    #    record_info("Typed 'alt-f4'", "");
+    #    sleep 1;
+    #    save_screenshot;
+    #    send_key "super-alt-f4";
+    #    record_info("Typed 'super-alt-f4'", "");
+    #    save_screenshot;
 
 
     #send_key "shift-f12";
     x11_start_program('vncviewer :0', target_match => 'virtman-gnome_virt-install', match_timeout => 100);
     # closing all windows
-    # julie debug
-    #send_key 'alt-f4' for (0 .. 2);
+    send_key 'alt-f4' for (0 .. 2);
     sleep 3600;
 }
 
