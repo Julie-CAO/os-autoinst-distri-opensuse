684b71c15ead95f7fbe6bdb9486c92322de0d28a
Julie-CAO <julie_caohe@163.com>
2025-06-16 19:06:38 +0800

    Close xterm console before opening vncviewer

--- tests/virtualization/virt_install.pm
+++ tests/virtualization/virt_install.pm
@@ -19,28 +19,22 @@ sub run {
     script_run('virt-install --name TESTING --osinfo detect=on,require=off --memory 512 --disk none --boot cdrom --graphics vnc &', 0);
     #script_run('virt-install --name TESTING --osinfo detect=on,require=off --memory 512 --disk none --boot cdrom --graphics vnc --noautoconsole &', 0);
     save_screenshot;
-    #    wait_still_screen(15);
     if (check_screen('allow-inhibiting-shortcuts', 10)) {
         send_key('left');
         send_key('ret');
         save_screenshot;
     }
+    wait_still_screen(15);
+    save_screenshot;
+    record_info("Typing 'alt-f4' to close current console", "");
+    send_key "alt-f4";
+    save_screenshot;
     #    send_key "super-esc" for (0 .. 2);
     #    record_info("Typed 3 times 'super'", "");
     #    sleep 1;
-    #    save_screenshot;
-    #    send_key "super-alt-f2";
-    #    record_info("Typed 'super-alt-f2'", "");
     #    sleep 1;
     #    save_screenshot;
-    #    send_key "alt-f4";
-    #    record_info("Typed 'alt-f4'", "");
-    #    sleep 1;
-    #    save_screenshot;
-    #    send_key "super-alt-f4";
-    #    record_info("Typed 'super-alt-f4'", "");
-    #    save_screenshot;
-    x11_start_program('vncviewer :0 -Shared', target_match => 'virtman-gnome_virt-install', match_timeout => 100);
+    x11_start_program('vncviewer :0', target_match => 'virtman-gnome_virt-install', match_timeout => 100);
     # closing all windows
     send_key 'alt-f4' for (0 .. 2);
 }
