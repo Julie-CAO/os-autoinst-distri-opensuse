e662e9a953ddfe1e65adbf5fe3c25fd6cb6d1d6b
Julie-CAO <julie_caohe@163.com>
2025-06-16 10:57:50 +0800

    Deny entering virt-viewer

--- tests/virtualization/virt_install.pm
+++ tests/virtualization/virt_install.pm
@@ -20,7 +20,7 @@ sub run {
     #script_run('virt-install --name TESTING --osinfo detect=on,require=off --memory 512 --disk none --boot cdrom --graphics vnc --noautoconsole &', 0);
     wait_still_screen(15);
     if (check_screen('allow-inhibiting-shortcuts', 10)) {
-	    #        send_key('left');
+        send_key('left');
         send_key('ret');
         save_screenshot;
     }
